5f519c64a2df69fe91afdbbc2cfb9459f6f4b9ad
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2017-04-11 10:50:52 +0200

    Requete POST (nouvelle dependance NPM)

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -2,8 +2,18 @@ var User = require('../model/user');
 
 module.exports = function (server) {
 
+    // GET
     server.get('/users', function (request, response) {
         User.findAll().then(function (data) {
+            response.send(data);
+        }, function (data) {
+            response.send({ah: 'AH !', error: data});
+        });
+    });
+
+    // POST
+    server.post('/user', function (request, response) {
+        User.create(request.body).then(function (data) {
             response.send(data);
         }, function (data) {
             response.send({ah: 'AH !', error: data});
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 var http = require('http');
 var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
 
 var Model = require('./model');
 
 // Load resources to be accessed without specific services
 var server = express();
+server.use(bodyParser.urlencoded({extended: true}));
 server.use('/resources', express.static(path.resolve('../ihm/resources')));
 
 server.get('/', function (request, response) {
@@ -18,7 +20,6 @@ require('./controller/announceController')(server);
 require('./controller/announceTypeController')(server);
 require('./controller/cityController')(server);
 require('./controller/commentController')(server);
-
 
 
 http.createServer(server).listen(8080, function () {

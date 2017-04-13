ded289a2a6d0b488df505c9493e2d37cea49a59a
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2017-04-13 08:59:53 +0200

    Workflow basique transaction (non securise)

--- server/controller/transactionController.js
+++ server/controller/transactionController.js
@@ -23,30 +23,88 @@ module.exports = function (server) {
     // POST
     server.post('/transaction', function (request, response) {
         var body = request.body;
-        var announce = Announce.findById(body.announceId);
-        if (announce.sale) {
-            body.sellerId = announce.authorId;
-            body.buyerId = body.accepterId;
-            body.buyerOk = true;
-            body.sellerOk = false;
-        } else {
-            body.buyerId = announce.authorId;
-            body.sellerId = body.accepterId;
-            body.sellerOk = true;
-            body.buyerOk = false;
-        }
-        body.transactionDate = (new Date()).getTime();
-        body.status = 0;
-        Transaction.create(body).then(function (data) {
+        Announce.findById(body.announceId).then(function (announce) {
+            if (announce.sale) {
+                body.sellerId = announce.authorId;
+                // Temp
+                body.buyerId = body.accepterId;
+
+                body.buyerOk = true;
+                body.sellerOk = false;
+            } else {
+                body.buyerId = announce.authorId;
+                //Temp
+                body.sellerId = body.accepterId;
+
+                body.sellerOk = true;
+                body.buyerOk = false;
+            }
+            body.transactionDate = (new Date()).getTime();
+            body.status = 0;
+            Transaction.create(body).then(function (data) {
+                response.send(data);
+            }, function (data) {
+                response.send({ah: 'AH !', error: data});
+            });
+        });
+    });
+
+    // PUT
+    server.put('/transaction/:id/accept', function (request, response) {
+        // Temp
+        var id = request.body.accepterId;
+
+        var body = {};
+
+        Transaction.findById(request.params.id).then(function (transaction) {
+            if (transaction.sellerId == id) {
+                body.sellerOk = true;
+            } else {
+                body.buyerOk = true;
+            }
+            body.status = 1;
+            Transaction.update(body, {where: {id: request.params.id}}).then(function (data) {
+                response.send(data);
+            }, function (data) {
+                response.send({ah: 'AH !', error: data});
+            });
+        });
+    });
+
+    server.put('/transaction/:id/end', function (request, response) {
+        var body = {
+            status: 2,
+            code: Math.floor(Math.random() * 100000)
+        };
+        Transaction.update(body, {where: {id: request.params.id}}).then(function (data) {
             response.send(data);
         }, function (data) {
             response.send({ah: 'AH !', error: data});
         });
     });
 
-    // PUT
-    server.put('/transaction', function (request, response) {
-        Transaction.update(request.body, {where: {id: request.body.id}}).then(function (data) {
+    server.put('/transaction/:id/close', function (request, response) {
+        var body = {
+            status: 3
+        };
+
+        Transaction.findById(request.params.id).then(function (transaction) {
+            if (request.body.code == transaction.code) {
+                Transaction.update(body, {where: {id: request.params.id}}).then(function (data) {
+                    response.send(data);
+                }, function (data) {
+                    response.send({ah: 'AH !', error: data});
+                });
+            } else {
+                response.send({ah: 'AH !', error: 'Code erronné !'});
+            }
+        });
+    });
+
+    server.put('/transaction/:id/cancel', function (request, response) {
+        // Temp
+        var body = {status: -1};
+        Transaction.update(body, {where: {id: request.params.id}}).then(function (data) {
             response.send(data);
         }, function (data) {
             response.send({ah: 'AH !', error: data});

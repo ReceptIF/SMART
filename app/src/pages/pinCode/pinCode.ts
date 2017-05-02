5379e53a9dc693899ef53065ef46be8f9a0863fc
Théo THIBAULT <tthibault@localhost.localdomain>
2017-05-02 09:51:59 +0200

    Remove pinCode from pinCode

--- app/src/pages/pinCode/pinCode.ts
+++ app/src/pages/pinCode/pinCode.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { UserProvider } from '../../providers/users.provider';
 
 @Component({
   selector: 'page-pinCode',
-  templateUrl: 'pinCode.html'
+  templateUrl: 'pinCode.html',
+  providers: [UserProvider]
 })
 export class PinCodePage {
 
   service : Service;
   answer : any;
+  connectedUser : any;
 
-	constructor(public navCtrl: NavController, public params:NavParams) {
+	constructor(public navCtrl: NavController, public params:NavParams,
+  private userProvider:UserProvider) {
 		
 		this.service = params.get("service");
 		this.answer = params.get("answer");
+    
+    this.userProvider.getConnectedUser().then(user => {
+			this.connectedUser = user;
+		});
     
 	}
   

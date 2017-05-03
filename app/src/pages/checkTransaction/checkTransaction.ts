bcd874d93b6963d6222aa8a6d28418161801fe83
Olivice <olivier.vicente95@gmail.com>
2017-05-03 18:59:26 +0200

    Fix checktransactions display bugs

--- app/src/pages/checkTransaction/checkTransaction.ts
+++ app/src/pages/checkTransaction/checkTransaction.ts
@@ -10,41 +10,41 @@ import { TransactionProvider } from '../../providers/transactions.provider';
   providers: [TransactionProvider]
 })
 export class CheckTransactionPage {
-	
+
 	service: Service;
   answers : any;
 
-	constructor(public navCtrl: NavController, public params:NavParams, 
+	constructor(public navCtrl: NavController, public params:NavParams,
         private modalCtrl: ModalController, private transactionProvider: TransactionProvider,
         private events : Events) {
-		
+
     this.service = params.get("service");
     this.answers = [];
-    
-    this.transactionProvider.getTransactionsByAnnounce(this.service.id).then( 
-      transactions => { 
+
+    this.transactionProvider.getTransactionsByAnnounce(this.service.id).then(
+      transactions => {
         this.answers = transactions;
         this.answers = this.answers.filter( transaction => {
 						return transaction.status == 0;
         });
     });
-    
+
     this.events.subscribe('reloadCheckTransactionPage',() => {
      this.navCtrl.pop();
      this.navCtrl.push(CheckTransactionPage, { service : this.service });
     });
-    
+
 	}
-  
+
   openModal(answer) {
     let modal = this.modalCtrl.create(TransactionModal,{answer: answer});
     modal.present();
   }
-  
+
   refuse(answer) {
 		this.transactionProvider.cancelTransaction(answer.id).then(
       response => {console.log(response)
     });
   }
-  
+
 }

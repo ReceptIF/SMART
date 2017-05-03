29ac8ed48797905a18dffc6efec95510bdf82d09
Olivice <olivier.vicente95@gmail.com>
2017-05-03 21:21:24 +0200

    Fix view profile on checktransaction

--- app/src/pages/pinCode/pinCode.ts
+++ app/src/pages/pinCode/pinCode.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, Events } from 'ionic-angular';
+
+import { ProfilePage } from '../profile/profile';
+
 import { UserProvider } from '../../providers/users.provider';
 import { TransactionProvider } from '../../providers/transactions.provider';
 
@@ -20,20 +23,20 @@ export class PinCodePage {
 	constructor(public navCtrl: NavController, public params:NavParams,
   private userProvider:UserProvider, private transactionProvider : TransactionProvider,
   private events : Events) {
-		
+
 		this.service = params.get("service");
 		this.answer = params.get("answer");
-    
+
     this.userProvider.getConnectedUser().then(user => {
 			this.connectedUser = user;
 		});
-    
+
 	}
-  
+
   valid() {
-  
+
     var commentBody = { title : this.title, content : this.comment, note : this.note };
-  
+
     if(this.connectedUser.id == this.answer.seller.id) {
       this.transactionProvider.endTransaction(this.answer.id,commentBody).then(
         transaction => {
@@ -43,12 +46,18 @@ export class PinCodePage {
       );
     } else if(this.connectedUser.id == this.answer.buyer.id) {
       this.transactionProvider.closeTransaction(this.answer.id,commentBody).then(
-        transaction => { 
+        transaction => {
           this.navCtrl.pop();
           this.navCtrl.pop();
         }
       );
     }
   }
-  
+
+  openProfile(event, userId) {
+    this.navCtrl.push(ProfilePage, {
+      profileId: userId
+    });
+  }
+
 }
